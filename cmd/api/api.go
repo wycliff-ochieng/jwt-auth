5da4aa86b6b206a7ed6e045e3fe7684234666b23
wycliff-ochieng <wyckieochieng91@gmail.com>
2025-06-06 19:02:52 +0300

    configuring router

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -1,7 +1,15 @@
 package api
 
 import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
 	"github.com/gorilla/mux"
+	"github.com/wycliff-ochieng/database"
+	"github.com/wycliff-ochieng/handlers"
+	"github.com/wycliff-ochieng/service"
 )
 
 type APIServer struct {
@@ -15,8 +23,25 @@ func NewAPIServer(addr string) *APIServer {
 }
 
 func (s *APIServer) Run() {
+
+	l := log.New(os.Stdout, "AUTHETICATION IN PROGRESS", log.LstdFlags)
+
+	db, err := database.NewPostgres()
+	if err != nil {
+		fmt.Printf("failed to connect to db %v", err)
+	}
+
+	if err := db.Init(); err != nil {
+		panic(err)
+	}
+
+	userService := service.NewUserService(db)
+
+	uh := handlers.NewAuthHandle(l, &userService)
+
 	router := mux.NewRouter()
 
-	getRouter := router.Methods("GET").Subrouter()
-	getRouter.HandleFunc("/login", nil)
+	postRouter := router.Methods("POST").Subrouter()
+	postRouter.HandleFunc("/register", uh.RegisterUser)
+	http.ListenAndServe(s.addr, router)
 }
